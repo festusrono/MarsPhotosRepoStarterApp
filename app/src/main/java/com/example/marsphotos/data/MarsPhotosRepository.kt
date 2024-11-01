d2139cf13af72eaeb4c2c8b10f334d9501494ecc
festusrono <festusrono56@gmail.com>
2024-11-01 09:22:29 +0100

    interface override getPhotos

--- app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
+++ app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
@@ -1,7 +1,16 @@
 package com.example.marsphotos.data
 
 import com.example.marsphotos.model.MarsPhoto
+import com.example.marsphotos.network.MarsApi
 
 interface MarsPhotosRepository {
+    //fetching list of MarsPhoto from marsApi
     suspend fun getMarsPhotos(): List<MarsPhoto>
+}
+
+class NetworkMarsPhotosRepository() : MarsPhotosRepository {
+    override suspend fun getMarsPhotos(): List<MarsPhoto> {
+        return MarsApi.retrofitService.getPhotos()
+    }
+
 }
