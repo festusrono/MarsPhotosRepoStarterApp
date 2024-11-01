9da50e27b6afaf2af267b71a1d00891b0787eae6
festusrono <festusrono56@gmail.com>
2024-11-01 19:43:08 +0100

    adding repository and dependency injections and viewModel updates

--- app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
+++ app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
@@ -1,16 +1,15 @@
 package com.example.marsphotos.data
 
 import com.example.marsphotos.model.MarsPhoto
-import com.example.marsphotos.network.MarsApi
+import com.example.marsphotos.network.MarsApiService
 
 interface MarsPhotosRepository {
     //fetching list of MarsPhoto from marsApi
     suspend fun getMarsPhotos(): List<MarsPhoto>
 }
 
-class NetworkMarsPhotosRepository() : MarsPhotosRepository {
-    override suspend fun getMarsPhotos(): List<MarsPhoto> {
-        return MarsApi.retrofitService.getPhotos()
-    }
-
+class NetworkMarsPhotosRepository(
+    private val marsApiService: MarsApiService
+) : MarsPhotosRepository {
+    override suspend fun getMarsPhotos(): List<MarsPhoto> = marsApiService.getPhotos()
 }
