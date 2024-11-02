5ae108bfa9ccbb7077b17ad98e9faef7c70aec83
festusrono <festusrono56@gmail.com>
2024-11-02 23:27:40 +0100

    updating the marsViewModelTest file in Test to assert that list contains photos

--- app/src/test/java/com/example/marsphotos/fake/MarsViewModelTest.kt
+++ app/src/test/java/com/example/marsphotos/fake/MarsViewModelTest.kt
@@ -15,13 +15,12 @@ class MarsViewModelTest {
 
 @Test
 fun marsViewModel_getMarsPhotos_verifyMarsUiStateSuccess() =
-    runTest{
+    runTest {
         val marsViewModel = MarsViewModel(
             marsPhotosRepository = FakeNetworkMarsPhotosRepository()
         )
         assertEquals(
-            MarsUiState.Success("Success: ${FakeDataSource.photosList.size} Mars photos retrieved"),
+            MarsUiState.Success(FakeDataSource.photosList),
             marsViewModel.marsUiState
         )
-
     }
